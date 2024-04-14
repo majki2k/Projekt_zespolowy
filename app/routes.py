6695c6310cc7fb593bdece20c94684723081575b
M4sterGumis <108536851+M4sterGumis@users.noreply.github.com>
2024-04-14 17:10:45 +0000

    routes edit

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,11 @@
-from flask import render_template
+
 from app import app
 from flask import Flask, render_template, request, redirect, url_for
-from flask_login import UserMixin, login_user
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
-def index():
-    return "Hello World"
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
-
-@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -38,18 +27,6 @@ def login():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
-        user = User.query.filter_by(email=email).first()
-
-        if user and user.password == password:
-            login_user(user)
-            return redirect(url_for('profile'))
-        else:
-            error_message = "Invalid login credentials. Please try again."
-            return render_template('login.html', error_message=error_message)
+        return redirect(url_for('index'))
 
     return render_template('login.html')
-
-if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
