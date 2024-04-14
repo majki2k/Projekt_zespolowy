03c1fc9554d6ebc587e5e490c62d4f3403bdb7ff
M4sterGumis <108536851+M4sterGumis@users.noreply.github.com>
2024-04-14 15:12:39 +0000

    forms translation

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SelectField, StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, NumberRange
+from wtforms import IntegerField, SelectField, StringField, PasswordField, SubmitField, BooleanField, TextAreaField # type: ignore
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError, NumberRange # type: ignore
 from flask import flash
-from flask_sqlalchemy import User, Book
-from wtforms.fields import DateField
+from flask_sqlalchemy import User
+from wtforms.fields import DateField # type: ignore
 from flask_wtf.file import FileField, FileAllowed
 from werkzeug.security import check_password_hash
 
@@ -11,51 +11,51 @@ class RegistrationForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    sex = SelectField('Płeć', choices=[('Mężczyzna', 'Mężczyzna'), ('Kobieta', 'Kobieta')])
+    sex = SelectField('Sex', choices=[('Man', 'Man'), ('Woman', 'Woman')])
     submit = SubmitField("Sign Up")
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
-            flash('Ten adres e-mail jest już zarejestrowany.')
-            raise ValidationError('Ten adres e-mail jest już zarejestrowany.')
+            flash('This email address is already registered.')
+            raise ValidationError('This email address is already registered.')
         if not any(char in ['@'] for char in email.data):
-            flash('Nieprawidłowy adres e-mail.')
-            raise ValidationError('Nieprawidłowy adres e-mail.')
+            flash('Incorrect e-mail address.')
+            raise ValidationError('Incorrect e-mail address.')
         if not any(char in ['.'] for char in email.data):
-            flash('Nieprawidłowy adres e-mail.')
-            raise ValidationError('Nieprawidłowy adres e-mail.')
+            flash('Incorrect e-mail address.')
+            raise ValidationError('Incorrect e-mail address.')
         if len(email.data) < 5:
-            flash('Nieprawidłowy adres e-mail.')
-            raise ValidationError('Nieprawidłowy adres e-mail.')
+            flash('Incorrect e-mail address.')
+            raise ValidationError('Incorrect e-mail address.')
         if len(email.data) > 30:
-            flash('Nieprawidłowy adres e-mail.')
-            raise ValidationError('Nieprawidłowy adres e-mail.')
+            flash('Incorrect e-mail address.')
+            raise ValidationError('Incorrect e-mail address.')
         if 'admin' in email.data:
-            flash('Nie można używać słowa "admin" w adresie e-mail.')
-            raise ValidationError('Nie można używać słowa "admin" w adresie e-mail.')
+            flash('You cannot use the word "admin" in your email address.')
+            raise ValidationError('You cannot use the word "admin" in your email address.')
             
     def validate_password(self, password):
         if len(password.data) < 8:
-            flash('Hasło musi mieć co najmniej 8 znaków.')
-            raise ValidationError('Hasło musi mieć co najmniej 8 znaków.')
+            flash('The password must be at least 8 characters long.')
+            raise ValidationError('The password must be at least 8 characters long.')
         if not any(char.isdigit() for char in password.data):
-            flash('Hasło musi zawierać co najmniej jedną cyfrę.')
-            raise ValidationError('Hasło musi zawierać co najmniej jedną cyfrę.')
+            flash('The password must contain at least one digit.')
+            raise ValidationError('The password must contain at least one digit.')
         if not any(char.isupper() for char in password.data):
-            flash('Hasło musi zawierać co najmniej jedną wielką literę.')
-            raise ValidationError('Hasło musi zawierać co najmniej jedną wielką literę.')
+            flash('The password must contain at least one uppercase letter.')
+            raise ValidationError('The password must contain at least one uppercase letter.')
         if not any(char.islower() for char in password.data):
-            flash('Hasło musi zawierać co najmniej jedną małą literę.')
-            raise ValidationError('Hasło musi zawierać co najmniej jedną małą literę.')
+            flash('The password must contain at least one lowercase letter.')
+            raise ValidationError('The password must contain at least one lowercase letter.')
         if not any(char in ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '='] for char in password.data):
-            flash('Hasło musi zawierać co najmniej jeden znak specjalny.')
-            raise ValidationError('Hasło musi zawierać co najmniej jeden znak specjalny.')
+            flash('The password must contain at least one special character.')
+            raise ValidationError('The password must contain at least one special character.')
 
     def validate_confirm_password(self, confirm_password):
         if confirm_password.data != self.password.data:
-            flash('Hasła muszą być takie same.')
-            raise ValidationError('Hasła muszą być takie same.')
+            flash('Passwords must be the same.')
+            raise ValidationError('Passwords must be the same.')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -65,25 +65,24 @@ class LoginForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
-            flash('Login jest nieprawidłowy.')
-            raise ValidationError('Login jest nieprawidłowy.')
+            flash('Login is incorrect.')
+            raise ValidationError('Login is incorrect.')
     
 
 
-class BookForm(FlaskForm):
+class ProductForm(FlaskForm):
     user = StringField('User', validators=[DataRequired()])
     product = StringField('ID', validators=[DataRequired()])
     order = StringField('ID', validators=[DataRequired()])
 
-class EditBookForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired(), Length(min=2, max=100)])
-    author = StringField('Author', validators=[DataRequired(), Length(min=2, max=100)])
+class EditProductForm(FlaskForm):
+    name = StringField('Title', validators=[DataRequired(), Length(min=2, max=100)])
     isbn = StringField('ISBN', validators=[Length(min=10, max=13)])
     description = TextAreaField('Description')
-    image = FileField('Book Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
+    image = FileField('Product Image', validators=[FileAllowed(['jpg', 'png'], 'Images only!')])
     current_quantity = IntegerField('Current Quantity', validators=[DataRequired(), NumberRange(min=0)])
     total_quantity = IntegerField('Total Quantity', validators=[DataRequired(), NumberRange(min=0)])
-    submit = SubmitField('Update Book')
+    submit = SubmitField('Update Product')
 
     def validate_total_quantity(self, total_quantity):
         if total_quantity.data < 0:
@@ -96,14 +95,14 @@ class EditBookForm(FlaskForm):
 class DeleteproductForm(FlaskForm):
     product = StringField('Product name', validators=[DataRequired()])
     confirm = BooleanField('Confirm Delete')
-    submit = SubmitField("Usuń produkt")
+    submit = SubmitField("Remove product")
 
 class DeleteUser(FlaskForm):
     user = StringField('User', validators=[DataRequired()])
     confirm = BooleanField('Confirm Delete')
-    submit = SubmitField("Usuń użytkownika")
+    submit = SubmitField("Delete user")
 
-class RateBookForm(FlaskForm):
+class RateProductForm(FlaskForm):
     rating = SelectField('Rating', choices=[(1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5')], coerce=int)
     submit = SubmitField('Rate')
 
