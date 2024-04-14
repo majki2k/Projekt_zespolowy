b6d1286dbf87b5e816136523433654cd84fbd16c
M4sterGumis <108536851+M4sterGumis@users.noreply.github.com>
2024-04-14 17:16:29 +0000

    routes indeks.html changed to base.html

--- app/routes.py
+++ app/routes.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
